b7845cbfea0658b2915f552273d6cbe7669662f1
Kristen (Isla) Lowe <144860115+islalowe@users.noreply.github.com>
2026-05-06 09:18:04 -0700

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 # This file handles constants like pin numbers, alarm time, brightness thresholds, and flash escalation intervals. 
 
-ALARM_HOUR   = 7
-ALARM_MINUTE = 30
+ALARM_HOUR   = 7      # this needs to be changeable by user
+ALARM_MINUTE = 30     # this needs to be changeable by user
 FLASH_COUNT  = 20
 FLASH_ON_MS  = 500
 FLASH_OFF_MS = 500
