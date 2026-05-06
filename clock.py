b445da0e129980aeca2d38386b1b0614113cafa9
Kristen (Isla) Lowe <144860115+islalowe@users.noreply.github.com>
2026-05-06 09:18:48 -0700

    Update clock.py

--- clock.py
+++ clock.py
@@ -1,13 +1,13 @@
  # This file contains timekeeping logic; tracks current time, alarm time, and handles the alarm trigger condition.
 
+#TODO change this file onve we incorporate RTC module
 
 import machine
-
 rtc = machine.RTC()
 
-def set_time(year, month, day, weekday, hour, minute, second=0):
-    rtc.datetime((year, month, day, weekday, hour, minute, second, 0))
+def set_time(year, month, day, hour, minute, second=0):
+    rtc.datetime((year, month, day, 0, hour, minute, second, 0))
 
 def get_time():
-    # returns (year, month, day, weekday, hour, minute, second, subsecond)
-    return rtc.datetime()
+    now = rtc.datetime()
+    return now[4], now[5]  # returns (hour, minute)
