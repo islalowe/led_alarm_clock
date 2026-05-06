3d20121b797be184bb69e97ef8f203b5279062df
Kristen (Isla) Lowe <144860115+islalowe@users.noreply.github.com>
2026-05-06 09:18:16 -0700

    Update display.py

--- display.py
+++ display.py
@@ -1,1 +1,11 @@
 # This file is the LCD driver wrapper; handles writing time, alarm status, and mode text to the screen.
+
+
+import machine
+import max7219
+from config import SPI_SCK, SPI_MOSI, CS_PIN
+
+spi = machine.SPI(0, sck=machine.Pin(SPI_SCK), mosi=machine.Pin(SPI_MOSI))
+cs  = machine.Pin(CS_PIN, machine.Pin.OUT)
+display = max7219.Matrix8x8(spi, cs, 4)
+display.brightness(15)
