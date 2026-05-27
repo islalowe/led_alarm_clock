f0ae0715ad7aa2032869c5dff1b32e0cc10b354c
Kristen (Isla) Lowe <144860115+islalowe@users.noreply.github.com>
2026-05-27 10:00:26 -0700

    Update clock.py

--- clock.py
+++ clock.py
@@ -1,13 +1,37 @@
- # This file contains timekeeping logic; tracks current time, alarm time, and handles the alarm trigger condition.
+import utime
 
-#TODO change this file onve we incorporate RTC module
+class Clock:
+    def __init__(self, hour, minute, second):
+        self.hour   = hour % 24
+        self.minute = minute % 60
+        self.second = second % 60
+        self._last_tick = utime.ticks_ms()
 
-import machine
-rtc = machine.RTC()
+    def tick(self):
+        now  = utime.ticks_ms()
+        diff = utime.ticks_diff(now, self._last_tick)
+        if diff >= 1000:
+            self._last_tick = utime.ticks_add(self._last_tick, 1000)
+            self.second += 1
+            if self.second >= 60:
+                self.second  = 0
+                self.minute += 1
+            if self.minute >= 60:
+                self.minute = 0
+                self.hour  += 1
+            if self.hour >= 24:
+                self.hour = 0
 
-def set_time(year, month, day, hour, minute, second=0):
-    rtc.datetime((year, month, day, 0, hour, minute, second, 0))
+    def time_str(self):
+        return "{:02d}:{:02d}:{:02d}".format(self.hour, self.minute, self.second)
 
-def get_time():
-    now = rtc.datetime()
-    return now[4], now[5]  # returns (hour, minute)
+    def matches(self, hour, minute):
+        return self.hour == hour and self.minute == minute and self.second == 0
+
+    def minutes_until(self, hour, minute):
+        now_total    = self.hour * 60 + self.minute
+        target_total = hour * 60 + minute
+        diff = target_total - now_total
+        if diff < -720:
+            diff += 1440
+        return diff
